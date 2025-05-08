546b70c437e0cbd5c630174d5ca582729abe746d
gabrielacarolline <gabrielacaroline1245@gmail.com>
2025-05-08 16:56:09 -0300

    Add files via upload

---
+++ exercicio 6.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i;  // Declara a variável do for
+    
+    for (i = 1; i <= 100; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0; // finaliza a programação.
+}
